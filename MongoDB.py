21d305d658c684f71255cb4f0d1590f5e65f9060
giovannacbs <86135899+giovannacbs@users.noreply.github.com>
2024-04-07 17:07:55 -0300

    Update MongoDB.py

--- MongoDB.py
+++ MongoDB.py
@@ -1,57 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+# Entering data into MongoDB
 
 import pandas as pd
-
-
-# In[5]:
-
-
 from pymongo import MongoClient
 
-
-# In[6]:
-
-
-# uri = [insert yours]
-# db = MongoClient(uri, ssl=True, tlsAllowInvalidCertificates=True)[yours]
-
-
-# In[7]:
-
-
-pnad = db.pnad23_gdcbs
-
-
-# In[2]:
-
+uri = [insert yours]
+db = MongoClient(uri, ssl=True, tlsAllowInvalidCertificates=True)[yours]
 
 df = pd.read_csv("pnad23.csv")
-
-
-# In[3]:
-
-
-df
-
-
-# In[4]:
-
-
 records = df.to_dict('records')
 
-
-# In[8]:
-
-
+pnad = db.pnad23_gdcbs
 pnad.insert_many(records)
-
-
-# In[9]:
-
-
-pnad.find_one()
-
